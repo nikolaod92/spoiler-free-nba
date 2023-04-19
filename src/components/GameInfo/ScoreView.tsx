06af296016d741ad7b04759a709e46ce76ed8477
NikolaOd <nikola.odalovic92@gmail.com>
2023-04-19 20:28:28 +0200

    Fix time display to show local time

--- src/components/GameInfo/ScoreView.tsx
+++ src/components/GameInfo/ScoreView.tsx
@@ -9,19 +9,19 @@ interface Props {
 
 const Score = ({ home, away, status, show }: Props) => {
   return (
-    <HStack justifyContent="center" backgroundColor="gray.200" mx={2} w={24} borderRadius={4}>
+    <HStack justifyContent='center' backgroundColor='gray.200' mx={2} w={24} borderRadius={4}>
       {show ? (
         <Flex gap={2}>
-          <Text fontSize={22} fontWeight="bold" color={home > away ? "green.500" : "gray.500"}>
+          <Text fontSize={22} fontWeight='bold' color={home > away ? "green.500" : "gray.500"}>
             {home}
           </Text>
-          <Text fontSize={22} fontWeight="bold" color={home < away ? "green.500" : "gray.500"}>
+          <Text fontSize={22} fontWeight='bold' color={home < away ? "green.500" : "gray.500"}>
             {away}
           </Text>
         </Flex>
       ) : (
-        <Text fontWeight="bold" color="gray.500">
-          {status.replace(" PM ET", "")}
+        <Text fontWeight='bold' color='gray.500'>
+          {new Date(status).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}
         </Text>
       )}
     </HStack>
