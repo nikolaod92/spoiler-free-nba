f634a05855496d5f52a8326c112bdec5338955aa
NikolaOd <nikola.odalovic92@gmail.com>
2023-04-19 22:06:24 +0200

    Remove unused code

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { Box, ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { Box, ChakraProvider } from "@chakra-ui/react";
 import "@fontsource/karla";
 import Navbar from "./components/Navbar";
 import theme from "./theme";
@@ -10,7 +10,7 @@ import theme from "./theme";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <Box h="full">
+      <Box h='full'>
         <Navbar />
         <App />
       </Box>
