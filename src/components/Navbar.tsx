2f66eccca5423cb165c91aab6a760e587a1c1143
NikolaOd <nikola.odalovic92@gmail.com>
2023-04-19 20:49:06 +0200

    Update navbar to be responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,35 +1,20 @@
-import {
-  Image,
-  Box,
-  Flex,
-  HStack,
-  Text,
-  IconButton,
-  useDisclosure,
-  useColorModeValue,
-  Link
-} from "@chakra-ui/react";
-import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+import { Image, Box, Flex, Text, useColorModeValue } from "@chakra-ui/react";
 
 export default function Navbar() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
   return (
-    <Box bg={useColorModeValue("gray.100", "gray.900")} w="full">
-      <Flex h={12} alignItems={"center"} justifyContent={"space-between"} w="4xl" m="auto">
-        <IconButton
-          size={"md"}
-          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-          aria-label={"Open Menu"}
-          display={{ md: "none" }}
-          onClick={isOpen ? onClose : onOpen}
-        />
-        <HStack w="full" alignItems="center" justifyContent="space-between">
-          <Text fontSize="xl" fontWeight="black" color="#1D4289">
-            Spoiler Free NBA
-          </Text>
-          <Image src="/nba-logo.png" w={10} />
-        </HStack>
+    <Box bg={useColorModeValue("gray.100", "gray.900")} w='full'>
+      <Flex
+        h={12}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        maxW='4xl'
+        m='auto'
+        px={{ base: 4, lg: 0 }}
+      >
+        <Text fontSize='xl' fontWeight='black' color='#1D4289'>
+          Spoiler Free NBA
+        </Text>
+        <Image src='/nba-logo.png' w={10} />
       </Flex>
     </Box>
   );
